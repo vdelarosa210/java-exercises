b3ba59d288e9740d10c20207db4133bbdfaa7ee3
violet <vdelarosa210@gmail.com>
2017-05-08 16:27:25 -0500

    Created pizza exercise

---
+++ src/ConsoleExercises/ConsoleExercises.java
@@ -0,0 +1,49 @@
+package ConsoleExercises;
+
+import java.util.Scanner;
+
+/**
+ * Created by violet on 5/8/17.
+ */
+public class ConsoleExercises {
+
+    public static void main(String[] args){
+
+     Scanner scan = new Scanner(System.in);
+
+     int zip;
+
+        System.out.println("What zip code do you live in: ");
+
+        zip = scan.nextInt();
+
+        System.out.println("You live in " + zip);
+
+    String firstName;
+    String middleName;
+    String lastName;
+
+        System.out.println("What is your full name?");
+        firstName = scan.next();
+        middleName = scan.next();
+        lastName = scan.nextLine();
+        System.out.println("Your name is " + firstName + " "  + middleName  + lastName);
+
+    String sentence;
+
+        System.out.println("Describe what your neighborhood is like in a complete sentence");
+        sentence = scan.nextLine();
+        System.out.println("You said, " + sentence);
+
+    int length;
+    int width;
+
+        System.out.println("Let's find out the size of your classroom. What is the length of your classroom in feet?");
+        length = scan.nextInt();
+        System.out.println("What is the width of the classroom in feet?");
+        width = scan.nextInt();
+        int perimeter = 2 * length + 2 * width;
+        System.out.println("So the area of your classroom is " + length * width + " squared feet." + " The perimeter of your classroom is " + perimeter + " feet");
+    }
+
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,39 @@
+/**
+ * Created by violet on 5/8/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        System.out.println("Hello again!");
+//        int myFavoriteNumber = 27;
+//        System.out.println(myFavoriteNumber);
+//        String myString = "This is how I write a sentence in Java.";
+//        System.out.println(myString);
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+//        myNumber = 123;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        type class;
+//        System.out.println(class);
+//        int x = 4 + 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
+
