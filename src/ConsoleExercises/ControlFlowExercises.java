caded7025dbfb34a1ec1979cf6d11aceb659539f
violet <vdelarosa210@gmail.com>
2017-05-10 10:00:08 -0500

    Altered loop to count backwards by 5's from 100 to -10.

--- src/ConsoleExercises/ControlFlowExercises.java
+++ src/ConsoleExercises/ControlFlowExercises.java
@@ -11,5 +11,16 @@ public class ControlFlowExercises {
             System.out.println(i);
             i+=2;
         }while (i <= 100);
+
+//first part of exercise//
+
+        i = 100;
+        do {
+            System.out.println(i);
+            i-=5;
+        }while (i >= -10);
+
     }
+
+
 }
