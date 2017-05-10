2202f7bc8c9bf3b2dcb8048b659ab62431e9cac1
violet <vdelarosa210@gmail.com>
2017-05-10 11:18:45 -0500

    Completed exercises with do while and for loop section

--- src/ConsoleExercises/ControlFlowExercises.java
+++ src/ConsoleExercises/ControlFlowExercises.java
@@ -6,40 +6,68 @@ package ConsoleExercises;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-//        //=====first part of exercise=====//
-//        int i = 0;
-//        do {
-//            System.out.println(i);
-//            i+=2;
-//        }while (i <= 100);
-//
-//        //=====second part of exercise=====//
-//
-//        i = 100;
-//        do {
-//            System.out.println(i);
-//            i-=5;
-//        }while (i >= -10);
-//
-//        //=====third part of exercise=====//
-//
-//        i = 100;
-//        do {
-//            System.out.println(i);
-//            i-=5;
-//        }while (i >= -10);
-//
-//        long n = 2;
-//
-//        do {
-//            System.out.println(n);
-//            n = n * n;
-//        }while(n < 1000000);
+        int i = 5;
+
+        //=====while loop exercise=====//
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
+
+        //=====first part of do while loop exercise=====//
+        i = 0;
+        do {
+            System.out.println(i);
+            i+=2;
+        }while (i <= 100);
+
+        //=====second part of do while loop exercise=====//
+
+        i = 100;
+        do {
+            System.out.println(i);
+            i-=5;
+        }while (i >= -10);
+
+        //=====third part of do while loop exercise=====//
+
+        i = 100;
+        do {
+            System.out.println(i);
+            i-=5;
+        }while (i >= -10);
+
+        long n = 2;
+
+        do {
+            System.out.println(n);
+            n = n * n;
+        }while(n < 1000000);
+
+//=====Create above exercises with For Loop=====//
+        //=====Part 1=====//
+        for (i = 0; i <= 100; i +=2) {
+            System.out.println(i);
+        }
+
+        //=====Part 2=====//
+
+        for (i = 100; i>= -10; i-=5){
+            System.out.println(i);
+        }
+
+        //=====Part 3=====//
+
+        long b = 2;
+
+        for (b = 2; b < 1000000; b = b * b){
+            System.out.println(b);
+        }
 
         //=====FizzBuzz test=====//
 
 
-        for (int i = 0; i <= 100; i++){
+        for (i = 0; i <= 100; i++){
             if (((i % 3) == 0) & ((i % 5) == 0)) {
                 System.out.println("fizzbuzz");
             }else if ((i % 3) == 0) {
@@ -48,5 +76,11 @@ public class ControlFlowExercises {
                 System.out.println("buzz");
             }else System.out.println(i);
         }
+
+        //=====Table of Powers=====//
+
+
+
+
     }
 }
