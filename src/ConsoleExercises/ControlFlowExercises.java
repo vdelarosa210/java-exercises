72fbe5e51bf85b085da06bf512863006c789cfd5
violet <vdelarosa210@gmail.com>
2017-05-10 10:06:14 -0500

    Created a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000.

--- src/ConsoleExercises/ControlFlowExercises.java
+++ src/ConsoleExercises/ControlFlowExercises.java
@@ -6,13 +6,14 @@ package ConsoleExercises;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
+        //first part of exercise//
         int i = 0;
         do {
             System.out.println(i);
             i+=2;
         }while (i <= 100);
 
-//first part of exercise//
+        //second part of exercise//
 
         i = 100;
         do {
@@ -20,7 +21,19 @@ public class ControlFlowExercises {
             i-=5;
         }while (i >= -10);
 
+        // third part of exercise//
+
+        i = 100;
+        do {
+            System.out.println(i);
+            i-=5;
+        }while (i >= -10);
+
+        long n = 2;
+
+        do {
+            System.out.println(n);
+            n = n * n;
+        }while(n < 1000000);
     }
-
-
 }
