45be6327bac8ef77bd709c6a96979ad9d1c79e41
violet <vdelarosa210@gmail.com>
2017-05-10 09:34:06 -0500

    Created a while loop, i with a value of 5, that runs so long as i is less than or equal to 15 Each loop iteration, output the current value of i, then incremented i by one.

---
+++ src/ConsoleExercises/ControlFlowExercises.java
@@ -0,0 +1,15 @@
+package ConsoleExercises;
+
+/**
+ * Created by violet on 5/10/17.
+ */
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
