15e2f1bf02d9f195398b493cbebaa91b7bd72402
violet <vdelarosa210@gmail.com>
2017-05-11 11:18:13 -0500

    Corrected the backslash section

--- src/ConsoleExercises/StringExercise.java
+++ src/ConsoleExercises/StringExercise.java
@@ -8,7 +8,8 @@ public class StringExercise {
         String message = ("We don't need no education \n" + "We don't need no thought control\n");
         message += ("Check \"this\" out!, \"s inside of \"s!\n");
         message += ("In windows, the main drive is usually C:'\'\n");
-        message += ("I can do backslashes '\', double backslashes '\\', and the amazing triple backslash '\\\'!");
+        message += ("I can do backslashes '\\', double backslashes '\\\\', and the amazing triple backslash '\\\\\\'!");
         System.out.println(message);
+
     }
 }
