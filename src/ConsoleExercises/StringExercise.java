12292f96be354ac336352bf8b1cf5a5727d6c0fa
violet <vdelarosa210@gmail.com>
2017-05-11 11:11:51 -0500

    Completed all exercises with concat for exercise

---
+++ src/ConsoleExercises/StringExercise.java
@@ -0,0 +1,14 @@
+package ConsoleExercises;
+
+/**
+ * Created by violet on 5/11/17.
+ */
+public class StringExercise {
+    public static void main(String[] args) {
+        String message = ("We don't need no education \n" + "We don't need no thought control\n");
+        message += ("Check \"this\" out!, \"s inside of \"s!\n");
+        message += ("In windows, the main drive is usually C:'\'\n");
+        message += ("I can do backslashes '\', double backslashes '\\', and the amazing triple backslash '\\\'!");
+        System.out.println(message);
+    }
+}
