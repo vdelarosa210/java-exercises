14cbccfd8be3914c447d9d663a2bd107a5184399
violet <vdelarosa210@gmail.com>
2017-05-12 12:31:31 -0500

    added do while loop to ask contact if they want to continue

--- src/ConsoleExercises/methods.java
+++ src/ConsoleExercises/methods.java
@@ -10,39 +10,49 @@ public class methods {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
 
-        int n1, n2;
-
+        String wantsToContinue;
         do {
-            n1 = askForNumber(data);
-        } while (confirm(n1) == false);
 
-        do {
-            n2 = askForNumber(data);
-        } while (confirm(n2) == false);
+            int n1, n2;
 
-        int result = addition(n1, n2);
-        System.out.println(n1 + " + " + n2 + " = " + result);
+            do {
+                n1 = askForNumber(data);
+            } while (confirm(n1) == false);
 
-        result = subtraction(n1, n2);
-        System.out.println(n1 + " - " + n2 + " = " + result);
+            do {
+                n2 = askForNumber(data);
+            } while (confirm(n2) == false);
 
-        result = multiplication (n1, n2);
-        System.out.println(n1 + " * " + n2 + " = " + result);
+            int result = addition(n1, n2);
+            System.out.println(n1 + " + " + n2 + " = " + result);
 
-        System.out.println(division(n1,n2));
+            result = subtraction(n1, n2);
+            System.out.println(n1 + " - " + n2 + " = " + result);
 
-        result = modulus(n1, n2);
-        System.out.println("The modulus of " + n1 + " & " + n2 + " = " + result);
+            result = multiplication(n1, n2);
+            System.out.println(n1 + " * " + n2 + " = " + result);
 
-        result = factorial(n1);
-        System.out.println("The factorial of " + n1 + " is " + result);
+            System.out.println(division(n1, n2));
+
+            result = modulus(n1, n2);
+            System.out.println("The modulus of " + n1 + " & " + n2 + " = " + result);
+
+            result = factorial(n1);
+            System.out.println("The factorial of " + n1 + " is " + result);
+
+    //=====ask if they want to continue=====//
+            System.out.println("Do you want to continue? (y/n)");
+            wantsToContinue = data.next();
+        } while (wantsToContinue.equalsIgnoreCase("y"));
 
     }
+
     //=====requests for a number to be entered=====//
     public static int askForNumber(Scanner input) {
         System.out.println("Enter a number between 1 and 10");
         return input.nextInt();
     }
+
     //=====confirms that the number is between 1-10=====//
     public static boolean confirm(int number) {
         if (number >= 1 && number <= 10) {
@@ -52,10 +62,12 @@ public class methods {
             return false;
         }
     }
+
     //=====addition=====//
     public static int addition(int n1, int n2) {
         return (n1 + n2);
     }
+
     //=====subtraction=====//
     public static int subtraction(int n1, int n2) {
         return (n1 - n2);
@@ -65,19 +77,21 @@ public class methods {
     public static int multiplication(int n1, int n2) {
         return (n1 * n2);
     }
+
     //=====division====//
     public static float division(int n1, int n2) {
-        return  (float) n1 / (float) n2;
+        return (float) n1 / (float) n2;
     }
+
     //=====modulus=====//
-    public static int modulus (int n1, int n2){
+    public static int modulus(int n1, int n2) {
         return (n1 % n2);
     }
 
     //=====factorial=====//
-    public static int factorial(int n1){
+    public static int factorial(int n1) {
         int result = 1;
-        for (int i = 1; i <= n1; i++){
+        for (int i = 1; i <= n1; i++) {
             result = result * i;
         }
         return (result);
