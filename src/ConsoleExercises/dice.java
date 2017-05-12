d76a854108d996b2c17aa86c9e3b49e94870ad31
violet <vdelarosa210@gmail.com>
2017-05-12 15:02:22 -0500

    Completed exercise that simulates dice rolling, need to create strings to display results.

--- src/ConsoleExercises/dice.java
+++ src/ConsoleExercises/dice.java
@@ -1,34 +1,35 @@
 package ConsoleExercises;
-//
-//import java.util.Scanner;
-//
-///**
-// * Created by violet on 5/12/17.
-// */
-//public class dice {
-//    public static void main(String[] args) {
-//
-//        Scanner data = new Scanner(System.in);
-//
-//        String rollAgain;
-//
-//        public static int sidesOfDice(Scanner input){
-//            System.out.println("Please enter a number of sides for a pair of dice?");
-//            return input.nextInt();
-//        }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    }
-//
-//}
+
+import java.util.Scanner;
+
+/**
+ * Created by violet on 5/12/17.
+ */
+public class dice {
+    public static void main(String[] args) {
+
+        Scanner data = new Scanner(System.in);
+
+        String rollAgain;
+
+        do{
+            int number = sidesOfDice(data);
+            System.out.println(roll(number));
+            System.out.println(roll(number));
+            System.out.println("Do you want to roll the dice again? (y/n)");
+            rollAgain = data.next();
+        }while (rollAgain.equalsIgnoreCase("y"));
+    }
+        //=====Number of Sides to the Dice=====//
+
+        public static int sidesOfDice(Scanner input){
+            System.out.println("Please enter a number of sides for a pair of dice?");
+            return input.nextInt();
+        }
+
+        //=====Roll the dice to create a random number=====//
+
+        public static int roll(int sides){
+            return 1 + (int)(Math.random() * sides);
+        }
+}
