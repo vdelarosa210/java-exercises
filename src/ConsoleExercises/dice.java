62178a33289673cf254fae34dfa4896a127456a4
violet <vdelarosa210@gmail.com>
2017-05-12 15:15:52 -0500

    Created strings to inform of which number goes with each dice

--- src/ConsoleExercises/dice.java
+++ src/ConsoleExercises/dice.java
@@ -14,21 +14,18 @@ public class dice {
 
         do{
             int number = sidesOfDice(data);
-            System.out.println(roll(number));
-            System.out.println(roll(number));
+            System.out.println("The number for the first dice is " + (roll(number)));
+            System.out.println("The number for the second dice is " + (roll(number)));
             System.out.println("Do you want to roll the dice again? (y/n)");
             rollAgain = data.next();
         }while (rollAgain.equalsIgnoreCase("y"));
     }
         //=====Number of Sides to the Dice=====//
-
         public static int sidesOfDice(Scanner input){
             System.out.println("Please enter a number of sides for a pair of dice?");
             return input.nextInt();
         }
-
         //=====Roll the dice to create a random number=====//
-
         public static int roll(int sides){
             return 1 + (int)(Math.random() * sides);
         }
