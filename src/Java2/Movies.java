3ed69a316e14f9b3a71343ca52dc493944679fe6
violet <vdelarosa210@gmail.com>
2017-05-18 14:47:33 -0500

    Created Movie Class and downloaded MoviesArray file. Also created constructors

---
+++ src/Java2/Movies.java
@@ -0,0 +1,23 @@
+package Java2;
+
+/**
+ * Created by violet on 5/18/17.
+ */
+public class Movies {
+
+    private String name;
+
+    private String category;
+
+    //=====constructor=====//
+    public MoviesArray (String name){
+        this.name = name;
+    }
+
+    public MoviesArray (String category){
+        this.category = category;
+    }
+
+}
+
+
