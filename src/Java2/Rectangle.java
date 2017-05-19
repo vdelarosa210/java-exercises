26624a8b550a0e3f9be7ab4ece776dab756c9b9b
violet <vdelarosa210@gmail.com>
2017-05-19 11:58:27 -0500

    Created square class and constructor for side by calling parent

--- src/Java2/Rectangle.java
+++ src/Java2/Rectangle.java
@@ -7,16 +7,18 @@ public class Rectangle {
     private int length;
     private int width;
 
-
+    //=====Constructor=====//
     public Rectangle(int length, int width) {
         this.length = length;
         this.width = width;
     }
 
+    //=====Perimeter formula=====//
     public static int getPerimeter(int length, int width) {
         return 2 * length + 2 * width;
     }
 
+    //=====Area formula=====//
     public static int getArea(int length, int width) {
         return length * width;
     }
--- src/Java2/Square.java
+++ src/Java2/Square.java
@@ -4,4 +4,17 @@ package Java2;
  * Created by violet on 5/19/17.
  */
 public class Square extends Rectangle {
+
+
+    //=====Constructor from parent=====//
+    public Square(int length, int width) {
+        super(length, width);
+    }
+
+    //=====side Method=====//
+
+    public static int side(){
+
+    }
+
 }
