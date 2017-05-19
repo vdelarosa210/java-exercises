afc3fd9d45ccce735389c5c65e0b2d4f46000e23
violet <vdelarosa210@gmail.com>
2017-05-19 12:18:30 -0500

    * Created a variable of the type Rectangle named box2 and assigned it a new instance of the Square class that has a side value of 5. * Verified that the getPerimeter and getArea methods returned 20 and 25, respectively.

--- src/Java2/ShapesTest.java
+++ src/Java2/ShapesTest.java
@@ -10,5 +10,9 @@ public class ShapesTest {
 
         System.out.println("This box measures " + box1.getPerimeter() + " " + box1.getArea());
 
+        Rectangle box2 = new Square(5);
+
+        System.out.println("This square box measures " + box2.getArea() + " " + box2.getPerimeter());
+
     }
 }
