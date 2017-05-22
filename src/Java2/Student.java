dd7a21a5b94e501acd8a2392f7e8d195a4603527
violet <vdelarosa210@gmail.com>
2017-05-22 14:32:43 -0500

    *Created grade collections

--- src/Java2/Student.java
+++ src/Java2/Student.java
@@ -1,5 +1,7 @@
 package Java2;
 
+import java.util.ArrayList;
+
 /**
  * Created by violet on 5/22/17.
  */
@@ -8,8 +10,19 @@ public class Student {
     private String studentName;
     private int studentGrade;
 
-    //=====Constructor=====//
+    public static void main(String[] args) {
 
+        ArrayList<Integer> grades = new ArrayList<Integer>();
+        grades.add(78);
+        grades.add(89);
+        grades.add(90);
+        System.out.println(grades.size());
+
+    }
+
+
+
+    //=====Constructor=====//
     public Student(String studentName, int studentGrade) {
         this.studentName = studentName;
         this.studentGrade = studentGrade;
