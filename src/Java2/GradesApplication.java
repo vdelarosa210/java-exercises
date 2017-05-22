7a441db36d0bf76dff178828fe78234bde87deff
violet <vdelarosa210@gmail.com>
2017-05-22 16:55:42 -0500

    Create y/n method loop asking if they want to see more information about another student
    TO DO:
    Create methods to choose which student they would like more information from
    
    Create confirm method when they enter something that does not equal the student

--- src/Java2/GradesApplication.java
+++ src/Java2/GradesApplication.java
@@ -3,12 +3,15 @@ package Java2;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by violet on 5/22/17.
  */
 public class GradesApplication {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
 
         ArrayList<Student> Students = new ArrayList<Student>();
 
@@ -28,15 +31,31 @@ public class GradesApplication {
         students.put("Jovani", 88);
         students.put("Jonathan", 95);
 
+
+
         System.out.println("Welcome!");
         System.out.println("Here are the github usernames of our students:");
         System.out.println(students.keySet());
         System.out.println("Which student would you like to know more information about?");
 
+        System.out.println("Would you like to see another student? (y/n)");
+
     }
 
+    public static Boolean confirm(Scanner input, String message) {
+        System.out.println(message);
+        String yn = input.next();
+
+        if (!yn.equalsIgnoreCase("y") && !yn.equalsIgnoreCase("n")) {
+
+            System.out.println("Sorry, I didn't catch that.");
+            return confirm(input, message);
+            // ask question again
+        }
+        return yn.equalsIgnoreCase("y");
+    }}
 
 
 
 
-}
+
